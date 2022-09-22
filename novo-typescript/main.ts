a0832d79f69e5fcffedc0e41876974df4524f208
matraf123 <matraf123@gmail.com>
2022-09-22 16:48:05 -0300

    first commit

---
+++ novo-typescript/main.ts
@@ -0,0 +1,24 @@
+import { NumberSymbol } from "@angular/common";
+
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+  return x + y;
+}
+//ES 6 OU ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+  return valor * 2;
+});
+numeros.map(valor => valor * 2); //es
+
+class Matematica{
+  soma(x, y){
+    return x + y;
+  }
+}
+var n1:any = 'asdasd';
+n1 = 4;
